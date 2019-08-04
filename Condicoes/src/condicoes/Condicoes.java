db1e030bc8a629159a3f6313bf041a2c2aa726c5
Fernando Baron <fernandobaron1@hotmail.com>
2019-08-03 21:31:31 -0300

    Modelo2

--- Condicoes/src/condicoes/Condicoes.java
+++ Condicoes/src/condicoes/Condicoes.java
@@ -14,6 +14,7 @@ exame
 
 abaixo de 4 reprovado*/
 
+/*        
  int nota1 = 5;
         int nota2 = 5;
         int media = (nota1 + nota2) /2;
@@ -25,6 +26,22 @@ abaixo de 4 reprovado*/
             System.out.println("Exame");
         } else {
         System.out.println("Reprovado");
+*/   
+        //MODELO 2
+
+ int nota1 = 5;
+        int nota2 = 5;
+        int media = (nota1 + nota2) /2;
+        int faltas = 12;
+        
+                
+        if (media >= 7 & faltas <=10) {
+            System.out.println ("Aprovado");
+        } else if ((media >= 4 & media <= 6)& faltas <= 10) {
+            System.out.println("Exame");
+        } else {
+        System.out.println("Reprovado");
+        
         }
     }
     
